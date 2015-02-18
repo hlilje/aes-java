4c8876f9384b87ea25f15e938fdae00107f8e1f4
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-18 21:38:09 +0100

    Implement plain state creation

--- AES.java
+++ AES.java
@@ -17,9 +17,35 @@ public class AES {
     private static byte[] key       = new byte[LENGTH_KEY];     // Encryption key
     private static byte[] plainText = new byte[LENGTH_DATA];    // Unencrypted bytes
     private static byte[] w         = new byte[LENGTH_EXP_KEY]; // Expanded cipher key
+    private static byte[][] states;                             // State 'matrices'
+
+
+    /* 
+     * Split the given plain text into an array of states, padding
+     * is added as necessary.
+     */
+    private static void createPlainStates(int textLength) {
+        int stateLength = Nb * Nb;
+
+        // Pad with 0s to make it divisible into nb x nb states
+        int diff = textLength % stateLength;
+        if (diff != 0) textLength += (stateLength - diff);
+
+        states = new byte[textLength][stateLength];
+
+        // Split into multiple states if > state length
+        int k = 0;
+        for (int i; i < textLength; ++i) {
+            for (int j; j < stateLength; ++j) {
+                states[i][j] = plainText[k];
+                ++k;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
+        int textLength = 0; // Length of plain text
 
         // Read the bytes from stdin
         DataInputStream dis;
@@ -30,11 +56,21 @@ public class AES {
             }
             for (int i = 0; dis.available() > 0; ++i) {
                 plainText[i] = dis.readByte();
+                ++text_length;
             }
             dis.close();
         } catch (Exception e){
             e.printStackTrace();
         }
+
+        // Expand encryption key
+        Rijndael.expandKeys(key, w, Nk * 4, LENGTH_KEY, LENGTH_EXP_KEY);
+        // Split plain text into states
+        createStates();
+        // Encrypt the plain text (states)
+        c = encrypt();
+        // Append encrypted states into cipher text
+        createCipherText()
 
         // Print key and data
         for (byte b : key) sb.append(String.format("%02X ", b));
