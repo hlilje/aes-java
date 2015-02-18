be4c0b5dd05abdb6448ff2f2a211e9f003c16cd6
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-02-18 19:46:17 +0100

    Read from stdin

--- AES.java
+++ AES.java
@@ -3,22 +3,40 @@
  * Reads a binary file from stdin and outputs the result on stdout.
  * Spec: http://csrc.nist.gov/publications/fips/fips197/fips-197.pdf
  */
-import java.io.*;
+import java.io.DataInputStream;
+import java.util.ArrayList;
 
 
 public class AES {
-
     private static final int KEY_LENGTH = 16; // Key length in bytes
 
     public static void main(String[] args) {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String s;
+        ArrayList<Byte> key       = new ArrayList<Byte>();
+        ArrayList<Byte> plainText = new ArrayList<Byte>();
+        StringBuilder sb          = new StringBuilder();
+
+        // Read the bytes from stdin
+        DataInputStream dis;
         try {
-            while ((s = in.readLine()) != null && s.length() != 0)
-                System.out.println(s);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
+            dis = new DataInputStream(System.in);
+            for (int i = 0; dis.available() > 0 && i < KEY_LENGTH; ++i) {
+                key.add(dis.readByte());
+            }
+            while (dis.available() > 0) {
+                plainText.add(dis.readByte());
+            }
+            dis.close();
+        } catch (Exception e){
+            e.printStackTrace();
         }
+
+        // Print key and data
+        for (byte b : key)
+            sb.append(String.format("%02X ", b));
+        System.out.println(sb);
+        sb = new StringBuilder();
+        for (byte b : plainText)
+            sb.append(String.format("%02X ", b));
+        System.out.println(sb);
     }
 }
